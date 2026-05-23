4ebdea73aebfff4d2f5fd40a4640f2951aa58685
Pavel Konovalov <ljadrbln@gmail.com>
2026-05-23 15:49:26 +0300

    feat: add multiline product title rendering

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
+from reportlab.lib.utils import simpleSplit
 from reportlab.graphics.barcode import createBarcodeDrawing
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-
 
 LABEL_WIDTH = 58 * mm
 LABEL_HEIGHT = 40 * mm
@@ -79,6 +79,132 @@ def render_label_v01(
     )
 
 
+def render_label_v02(
+    pdf,
+    article,
+    name,
+    size,
+    price,
+    barcode_value
+):
+    margin = 2 * mm
+
+    inner_x = margin
+    inner_y = margin
+
+    inner_width = LABEL_WIDTH - (margin * 2)
+    inner_height = LABEL_HEIGHT - (margin * 2)
+
+    left_column_width = 10 * mm
+
+    # Vertical separator
+    pdf.line(
+        inner_x + left_column_width,
+        inner_y,
+        inner_x + left_column_width,
+        inner_y + inner_height
+    )
+
+    # LEFT COLUMN
+    # Vertical price
+    pdf.saveState()
+
+    pdf.setFont("DejaVu", 16)
+
+    price_x = inner_x + (left_column_width / 2)
+    price_y = inner_y + (inner_height / 2)
+
+    pdf.translate(
+        price_x,
+        price_y
+    )
+
+    pdf.rotate(90)
+
+    pdf.drawCentredString(
+        0,
+        0,
+        f"{price} грн"
+    )
+
+    pdf.restoreState()
+
+    # RIGHT COLUMN
+
+    right_x = inner_x + left_column_width
+    right_width = inner_width - left_column_width
+
+    # Store name
+    pdf.setFont("DejaVu", 8)
+
+    pdf.drawCentredString(
+        right_x + (right_width / 2),
+        inner_y + inner_height - (5 * mm),
+        "COSMO"
+    )
+
+    # Product text
+    product_text = f"{article} {name} {size}"
+
+    pdf.setFont("DejaVu", 12)
+
+    max_text_width = right_width - (4 * mm)
+
+    lines = simpleSplit(
+        product_text,
+        "DejaVu",
+        12,
+        max_text_width
+    )
+
+    line_height = 5 * mm
+
+    block_height = len(lines) * line_height
+
+    start_y = (
+        inner_y
+        + (inner_height / 2)
+        + (block_height / 2)
+    )
+
+    for index, line in enumerate(lines):
+        y = start_y - (index * line_height)
+
+        pdf.drawCentredString(
+            right_x + (right_width / 2),
+            y,
+            line
+        )
+        
+    # product_text = f"{article} {name} {size}"
+
+    # pdf.setFont("DejaVu", 12)
+
+    # pdf.drawCentredString(
+    #     right_x + (right_width / 2),
+    #     inner_y + (inner_height / 2),
+    #     product_text
+    # )
+
+    # Barcode
+    barcode = createBarcodeDrawing(
+        'EAN13',
+        value=barcode_value,
+        barHeight=8 * mm,
+        humanReadable=True
+    )
+
+    barcode_width = 32 * mm
+
+    barcode_x = right_x + ((right_width - barcode_width) / 2)
+    barcode_y = inner_y + (2 * mm)
+
+    barcode.drawOn(
+        pdf,
+        barcode_x,
+        barcode_y
+    )
+
 def main():
     article = "227701"
     name = "ICEBERG білий"
@@ -105,7 +231,7 @@ def main():
         )
     )
 
-    render_label_v01(
+    render_label_v02(
         pdf,
         article,
         name,
